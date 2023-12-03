5aa33d5a91ff1f57995ee050f7f3ef23a4dfa408
shweta vangari <83689848+shwetavangari@users.noreply.github.com>
2023-12-03 16:30:35 +0530

    Add files via upload

---
+++ Ass25.js
@@ -0,0 +1,33 @@
+var btns=document.querySelector(".btns");
+
+var fname=document.querySelector("#fname");
+var lname=document.querySelector("#lname");
+var email=document.querySelector("#email");
+var mobile=document.querySelector("#mobile");
+
+btns.addEventListener("click",function(){
+    if(fname.value=="" || lname.value=="" || email.value=="" || mobile.value==""){
+        // console.log("Mandatory..")
+        errormsg.innerHTML="All fields are mandtory..";
+        errormsg.style.color="red"
+        if(fname.value==""){
+            fname.style.animation="errorAnimation linear 1s";
+            fname.classList.add("anim");
+        }
+        else if(lname.value==""){
+            lname.style.animation="errorAnimation linear 1s";
+            lname.classList.add("anim");
+        }
+        else if(email.value==""){
+            email.style.animation="errorAnimation linear 1s";
+            email.classList.add("anim");
+        }
+        else if(mobile.value==""){
+            mobile.style.animation="errorAnimation linear 1s";
+            mobile.classList.add("anim");
+        }
+    }else{
+        errormsg.style.color="green";
+        errormsg.innerHTML="Register Success...";
+    }
+})
